3d93cca0c83a3835e9ae5a5282417a41957b5c28
Elandrehs <andrestheb@gmail.com>
2026-03-25 16:25:09 -0500

    Initialization commit

---
+++ src/main/java/com/pezbackend/PezBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pezbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PezBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PezBackendApplication.class, args);
+    }
+
+}
