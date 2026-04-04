11794c315c3f031f1fe36d58802dd42faca6baf1
Elandrehs <andrestheb@gmail.com>
2026-04-04 15:13:21 -0500

    fix(billing): update resources and repository to support split payments correctly

--- src/main/java/com/pezbackend/billing/interfaces/rest/transform/SaleResourceFromEntityAssembler.java
+++ src/main/java/com/pezbackend/billing/interfaces/rest/transform/SaleResourceFromEntityAssembler.java
@@ -2,9 +2,10 @@ package com.pezbackend.billing.interfaces.rest.transform;
 
 import com.pezbackend.billing.domain.model.aggregates.Sale;
 import com.pezbackend.billing.domain.model.entities.SaleDetail;
+import com.pezbackend.billing.domain.model.entities.SalePayment;
 import com.pezbackend.billing.interfaces.rest.resources.SaleDetailResource;
+import com.pezbackend.billing.interfaces.rest.resources.SalePaymentResource;
 import com.pezbackend.billing.interfaces.rest.resources.SaleResource;
-
 import java.util.stream.Collectors;
 
 public class SaleResourceFromEntityAssembler {
@@ -18,11 +19,13 @@ public class SaleResourceFromEntityAssembler {
                 sale.getCustomerDni(),
                 sale.getCustomerRuc(),
                 sale.getDocumentType(),
-                sale.getPaymentMethod(),
                 sale.getTotal(),
                 sale.getCreatedAt(),
                 sale.getDetails().stream()
                         .map(SaleResourceFromEntityAssembler::toDetailResource)
+                        .collect(Collectors.toList()),
+                sale.getPayments().stream()
+                        .map(SaleResourceFromEntityAssembler::toPaymentResource)
                         .collect(Collectors.toList())
         );
     }
@@ -34,6 +37,13 @@ public class SaleResourceFromEntityAssembler {
                 detail.getQuantity(),
                 detail.getTotalPrice(),
                 detail.getNote()
+        );
+    }
+
+    private static SalePaymentResource toPaymentResource(SalePayment payment) {
+        return new SalePaymentResource(
+                payment.getMethod(),
+                payment.getAmount()
         );
     }
 }
